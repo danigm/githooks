ae96a851cc90a3184062cbea7b56274e81618c08
Daniel Garcia <danigm@yaco.es>
2010-06-25 14:27:54 +0200

    Using status --porcelain for better parsing

--- pep8-pyflakes-pre-commit.py
+++ pep8-pyflakes-pre-commit.py
@@ -4,25 +4,23 @@ import sys
 import re
 import subprocess
 
-modified = re.compile('modified:\s+(?P<name>.*\.py)')
-new = re.compile('new file:\s+(?P<name>.*\.py)')
+modified = re.compile('^(?:M|A)..(?P<name>.*\.py)')
+
 
 def main():
-    p = subprocess.Popen(['git', 'status'], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['git', 'status', '--porcelain'],
+                         stdout=subprocess.PIPE)
     out, err = p.communicate()
     modifieds = modified.findall(out)
-    news = new.findall(out)
-
-    modifieds += news
 
     rrcode = 0
-    for file in set(modifieds):
+    for file in modifieds:
         p = subprocess.Popen(['pep8', file],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out or err:
-            sys.stdout.write("pep8:\n%s\n%s" % (out, err))
+            sys.stdout.write(" * pep8:\n%s\n%s" % (out, err))
             rrcode = rrcode | 1
         retcode = subprocess.call(['pyflakes', file])
         rrcode = retcode | rrcode
